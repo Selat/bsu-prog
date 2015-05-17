4243e71870ca46579ebf8798572c64c336280dac
selat <selatnick@gmail.com>
2015-05-17 22:10:34 +0300

    Add operator< to the DBRecord class

--- database/dbrecord.cpp
+++ database/dbrecord.cpp
@@ -1,5 +1,12 @@
 #include "dbrecord.h"
 
-DBRecord::DBRecord()
+DBRecord::DBRecord(const std::vector <DBField> &fields, const std::vector<int> &key_fields) :
+	fields_(fields),
+	key_(fields, key_fields)
 {
+}
+
+bool DBRecord::operator<(const DBRecord &r) const
+{
+	return key_ < r.key_;
 }
--- database/dbrecord.h
+++ database/dbrecord.h
@@ -4,13 +4,16 @@
 #include <vector>
 
 #include "dbfield.h"
+#include "dbkey.h"
 
 class DBRecord
 {
 public:
-	DBRecord();
+	DBRecord(const std::vector <DBField> &fields, const std::vector <int> &key_fields);
+	bool operator<(const DBRecord &r) const;
 private:
 	std::vector <DBField> fields_;
+	DBKey key_;
 };
 
 #endif
