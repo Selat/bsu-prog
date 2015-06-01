a1291bec448bcdf228660606e59eb993956fc8e2
selat <selatnick@gmail.com>
2015-06-02 01:16:41 +0300

    Fix some bugs and add support of inserting spaces after semicolons

--- asm-space-remove/main.cpp
+++ asm-space-remove/main.cpp
@@ -20,10 +20,11 @@ using namespace std;
  * after each ';' - it constitues for the end of the sentence.
  */
 
-extern "C" void solve(char *s);
+extern "C" int solve(char *s);
 
 void runTests()
 {
+	// Testing just space removing
 	TESTCASE("1 25     ", "1 25");
 	TESTCASE("   1    23   3    5     ", "1 23 3 5");
 	TESTCASE("   1    23   3    5     ", "1 23 3 5");
@@ -39,7 +40,13 @@ void runTests()
 	TESTCASE("1 2 2  ", "1 2 2");
 	TESTCASE("", "");
 	TESTCASE("               ", "");
-	TESTCASE("                           MID              ", "MID");
+	// Testing space removing and addition of space after each semicolon
+	TESTCASE("1  ;", "1 ; ");
+	TESTCASE("  1;", "1; ");
+	TESTCASE("1   ;2", "1 ; 2");
+	TESTCASE("2  1;", "2 1; ");
+	TESTCASE(" ;", "; ");
+	TESTCASE("          ;;;;;;;;;;", "; ; ; ; ; ; ; ; ; ; ");
 	puts("The program successfully passed all testcases!");
 }
 
@@ -49,7 +56,7 @@ int main()
 	char buf[256];
 	fgets(buf, 256, stdin);
 	buf[strlen(buf) - 1] = '\0';
-	solve(buf);
-	printf("%s|\n", buf);
+	int n = solve(buf);
+	printf("%s|%d\n", buf, n);
 	return 0;
 }
--- asm-space-remove/space-remove.asm
+++ asm-space-remove/space-remove.asm
@@ -2,7 +2,7 @@
 
 	segment .data
 %define spacecode 32
-%define semicoloncode '\;'
+%define semicoloncode 59
 
 	segment .text
 
@@ -10,12 +10,18 @@
 
 solve:
 	push rdi
+	push rdi
 	call strlen
 	pop rdi
 	mov rcx, rax
 	inc rcx
 	mov al, spacecode
-	xor r9, r9					; (bool)R9 - found any non-space characters in the string.
+	xor r9, r9					; R9 : bool - found any non-space characters in the string.
+
+	;; Remove all unnecessary spaces
+	;; 'loop' cycle invariant:
+	;; RSI - current insert position
+	;; RDI - first space position
 
 	mov rsi, rdi
 	jrcxz end
@@ -41,7 +47,6 @@ loop:
 skip3:
 	;; We have found a non-space character, therefore we need to store 1 in R9.
 	mov r9, 1
-	push rcx
 	dec rdi						; After execution of repe function rdi points to the second symbol of the word
 	inc rcx
 	;; Find out the length of the current word
@@ -51,6 +56,7 @@ skip3:
 	dec rdi
 	inc rcx
 skip2:
+	push rcx
 	mov rcx, rdi
 	sub rcx, rbx
 	mov rdi, rbx
@@ -73,4 +79,74 @@ end:
 	dec rsi
 skip1:
 	mov byte [rsi], 0
+
+	;; Add one space after each semicolon
+	pop rdi
+	push rdi
+	call strlen
+	pop rdi
+	mov rcx, rax
+	push rcx
+
+	;; RBX stores number of spaces to add
+	push rdi
+	xor rbx, rbx
+	mov al, semicoloncode
+	inc rcx
+lcntsemicolon:
+	jrcxz endcntsemicolon
+	repne scasb
+	jnz lcntsemicolon
+	cmp byte [rdi], spacecode
+	je lcntsemicolon
+	inc rbx
+	jmp lcntsemicolon
+
+endcntsemicolon:
+	pop rdi
+	pop rcx
+	mov rsi, rdi
+	add rsi, rcx
+	dec rsi
+	add rdi, rcx
+	add rdi, rbx
+	dec rdi
+	mov byte [rdi + 1], 0
+	;; Set direction bit, because we neeed to decrement addresses
+	inc rcx
+	std
+lmovesemicolons:
+	cmp rbx, 0
+	je end2
+
+	;; Firstly we need to find out the length of the string that we should copy
+	push rsi
+	push rdi
+	xchg rsi, rdi
+	mov r9, rdi
+	repne scasb
+	sub r9, rdi
+	xchg rsi, rdi
+	dec r9
+	jrcxz skip4
+	;; dec r9
+skip4:
+	pop rdi
+	pop rsi
+	push rcx
+
+	;; Than we just copy an array of bytes of known length
+	mov rcx, r9
+	rep movsb
+	pop rcx
+	mov byte [rdi], spacecode
+	mov byte [rdi - 1], semicoloncode
+	dec rbx
+	dec rsi
+	sub rdi, 2
+
+	jmp lmovesemicolons
+end2:
+	cld
+	mov rax, rbx
 	ret
